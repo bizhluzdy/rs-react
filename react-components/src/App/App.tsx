0c4f1f7d87113f30ca4cd689b9e06df11bac7c11
bizhluzdy <murdrum22@gmail.com>
2023-10-30 12:41:52 +0100

    feat: complete task

---
+++ react-components/src/App/App.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Searcher from '../components/Searcher/Searcher';
+import Plate from '../components/Plate/Plate';
+
+interface AppState {
+  searchTerm: string;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      searchTerm: '',
+    };
+  }
+  updateSearchTerm = (searchTerm: string) => {
+    this.setState({ searchTerm });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>StarWars: Characters</h1>
+        <Searcher onSearch={this.updateSearchTerm} />
+        <Plate searchTerm={this.state.searchTerm} />
+      </div>
+    );
+  }
+}
+
+export default App;
